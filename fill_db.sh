a25986e3b60bf6b52dca3a7c4a630aa4319839cf
GONI Guillaume <gdn3@live.fr>
2019-01-12 13:33:41 +0100

    new script to read postcodes directly from nominatim db

--- fill_db.sh
+++ fill_db.sh
@@ -1,20 +1,6 @@
 #!/bin/sh
 
-# Codes from : http://download.geonames.org/export/zip/
-
-# It is a country code of two letters (i.e. `DE`) or `allCountries` for all of them.
-COUNTRY_CODE="$1"
-
-ZIPCODES_SOURCE="http://download.geonames.org/export/zip"
 TMP_FOLDER="$(pwd)/geonom/tmp"
-
-ARCHIVE_NAME="${COUNTRY_CODE}.zip"
-
-if [ ! -n "$COUNTRY_CODE" ]
-then
-    echo "Please provide a country code as parameter." 2>&1
-    exit 1
-fi
 
 # Creating tmp dir
 if [ ! -d "tmp" ]
@@ -31,12 +17,7 @@ cd $TMP_FOLDER
 # Cloning NominatimLibrary
 git clone http://github.com/dataBikeHsUlm/NominatimLibrary.git
 
-# Get zipcodes for the requested country code :
-wget "$ZIPCODES_SOURCE/$ARCHIVE_NAME"
-
-unzip "$ARCHIVE_NAME"
-
-cat fill_db.py | python3 ../manage.py shell
+python3 ../fill_db_from_osm.py
 
 cd ..
 rm -Rf $TMP_FOLDER
---
+++ fill_db_from_osm.py
@@ -0,0 +1,69 @@
+#!/bin/env python3
+from NominatimLibrary import Locator, NotFoundException
+import psycopg2
+import mysql.connector
+import sys
+
+# Nominatim postgreSQL
+DB_NOMINATIM_NAME = "Planet"
+DB_NOMINATIM_USER = "dataproject"
+# TODO: ORDER BY performance ?
+QUERY_POSTCODES_COUNTRIES = "SELECT DISTINCT country_code,postcode FROM location_area WHERE country_code IS NOT NULL AND postcode IS NOT NULL ORDER BY country_code, postcode;"
+
+# MySQL
+DB_MySQL_NAME = "geonom"
+DB_MySQL_USER = "admin"
+DB_MySQL_TABLE = "datamodel_zipcode"
+INSERT_INTO_MYSQL = "INSERT INTO " + DB_MySQL_TABLE + " VALUES (%s, %s, %s, %s);"
+
+# TODO: Better solution ? Read from stdin ?
+if len(sys.argv) > 1:
+    DB_MySQL_PASSWORD = sys.argv[1]
+else:
+    print("ERROR : No arguments. Usage : `script.py MYSQL_PASSWORD`", file=sys.stderr)
+    sys.exit(1)
+
+print("Connecting to PostgreSQL database...")
+postgres = psycopg2.connect('dbname=' + DB_NOMINATIM_NAME  + ' user=' + DB_NOMINATIM_USER)
+pg_cursor = conn.cursor()
+
+print("Connecting to MySQL database...")
+mydb = mysql.connector.connect(
+        host="localhost",
+        user=DB_MySQL_USER
+        passwd=DB_MySQL_PASSWORD
+)
+mydb_cursor = mydb.cursor()
+
+print("Executing query to get all postcodes per countries...")
+pg_cursor.execute(QUERY_POSTCODES_COUNTRIES)
+
+print("Executing query to get all postcodes per countries...")
+counter = 0
+while True:
+    elm = pg_cursor.fetchone()
+    if elm  == None:
+        break
+    else:
+        country_code = elm[0].lower()
+        zipcode = elm[1]
+
+        print(counter + " : " + country_code + " : " + zipcode)
+
+        # There already is a lon and lat in the file, use it ?
+        try:
+            # TODO: make more accurate queries ? (country name and city name)
+            (lat, lon) = locator.get_coordinates("" + country_code + "," + zipcode)
+        except NotFoundException:
+            print("ERROR : " + query + " : not found, skipping...", file=sys.stderr)
+            continue
+        except Exception as e:
+            print("ERROR : unknown error : " + e, file=sys.stderr)
+            continue
+
+        mysql.execute(INSERT_INTO_MYSQL, (counter, country_code, zipcode, 0, 0)
+        counter += 1
+
+print("Stopped at zipcode nb : " + counter)
+postgres.close()
+mydb.commit()
