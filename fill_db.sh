d2a442cafebfdbe0dd93d75809132c2343c5d74f
GONI Guillaume <gdn3@live.fr>
2019-02-21 18:24:01 +0100

    typo

--- fill_db.sh
+++ fill_db.sh
@@ -7,6 +7,6 @@ rm -Rf NominatimLibrary
 git clone http://github.com/dataBikeHsUlm/NominatimLibrary.git
 
 # python3 fill_db_from_osm.py
-python3 fill_db_with distances.py
+python3 fill_db_with_distances.py
 
 rm -Rf NominatimLibrary
