c68a0fa387372a28be03b8a4256e0046a68d2893
GONI Guillaume <gdn3@live.fr>
2019-03-12 11:50:38 +0100

    another stupid mistake

--- fill_db_with_distances_2digits.py
+++ fill_db_with_distances_2digits.py
@@ -55,7 +55,7 @@ for city in elms:
     lon = city[4]
     if lat >= LAT_MIN and lat <= LAT_MAX and lon >= LON_MIN and lon <= LON_MAX:
         n,sum_lat,sum_lon = res[(country_2digits, lat, lon)]
-        res[(country_2digits, lat, lon)] = (n+1,sum_lat+lat,sum_lon+lon)
+        res[country_2digits] = (n+1,sum_lat+lat,sum_lon+lon)
 
 keys = res.keys()
 print("We are using " + str(len(keys)) + " areas.")
