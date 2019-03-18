67bde9be0b9256b0da823e0d08883a83147bbaa9
GONI Guillaume <gdn3@live.fr>
2019-03-18 09:59:51 +0100

    fill_db_with_distances grid 0.1

--- fill_db_with_distances.py
+++ fill_db_with_distances.py
@@ -10,13 +10,14 @@ LON_MIN = -11
 LON_MAX = 41
 LAT_MIN = 35
 LAT_MAX = 71
-CENTROID_SHIFT = 0.5
+STEP = float(0.1)
+CENTROID_SHIFT = STEP/2.0
 
 # MySQL
 DB_MySQL_NAME = "geonom"
 DB_MySQL_USER = "admin"
 DB_MySQL_TABLE_ZIP = "datamodel_zipcode"
-DB_MySQL_TABLE_DIST = "datamodel_zipdist"
+DB_MySQL_TABLE_DIST = "datamodel_zipdist_grid_10"
 INSERT_INTO_MYSQL = "INSERT INTO " + DB_MySQL_TABLE_DIST + " VALUES (id, %s, %s, %s, %s, %s, %s);"
 
 QUERY_POSTCODES_COUNTRIES = "SELECT * FROM " + DB_MySQL_TABLE_ZIP + ";"
@@ -25,6 +26,8 @@ print(" --------------------------------- ")
 DB_MySQL_PASSWORD = input("MySQL password : ")
 
 locator = Locator()
+
+##################################################
 
 def connect_mydb():
     print("Connecting to MySQL database...")
@@ -42,12 +45,14 @@ print("Executing query to get all postcodes per countries...")
 mydb_cursor.execute(QUERY_POSTCODES_COUNTRIES)
 elms = mydb_cursor.fetchall()
 
+##################################################
+
 print("Filtering squares containing postcodes...")
 
 res = {}
 for city in elms:
-    lat = math.floor(city[3])
-    lon = math.floor(city[4])
+    lat = math.floor(city[3] / STEP)
+    lon = math.floor(city[4] / STEP)
     if lat >= LAT_MIN and lat <= LAT_MAX and lon >= LON_MIN and lon <= LON_MAX:
         res[(lat,lon)] = True
 
@@ -56,18 +61,20 @@ print("We are using " + str(len(keys)) + " instead of " + str((LON_MAX-LON_MIN)*
 nb_paths = math.factorial(len(keys))/(math.factorial(2)*math.factorial(len(keys)-2))
 print("This makes a total number of paths of : " + str(nb_paths))
 
+##################################################
+
 print("Calculating distances between squares...")
 counter = 0
 
 for (a_id, a_square) in enumerate(keys):
-    a_coords = (a_square[0] + CENTROID_SHIFT, a_square[1] + CENTROID_SHIFT)
+    a_coords = (a_square[0] * STEP + CENTROID_SHIFT, a_square[1] * STEP + CENTROID_SHIFT)
     for (b_id, b_square) in enumerate(keys):
         if b_id <= a_id:
             continue
 
         counter += 1
 
-        b_coords = (b_square[0] + CENTROID_SHIFT, b_square[1] + CENTROID_SHIFT)
+        b_coords = (b_square[0] * STEP + CENTROID_SHIFT, b_square[1] * STEP + CENTROID_SHIFT)
 
         d_crow = locator.distance_crow_coords(a_coords, b_coords);
 
