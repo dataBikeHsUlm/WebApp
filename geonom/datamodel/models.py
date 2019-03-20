47733b3c506caa558fdb91e7730ca67215f15ef2
GONI Guillaume <gdn3@live.fr>
2019-03-20 15:05:07 +0100

    write along tests

--- geonom/datamodel/models.py
+++ geonom/datamodel/models.py
@@ -5,6 +5,7 @@ from random import randrange
 
 # AVERAGE_RATIO is the average ratio dist_route/dist_crow on a random set of postcode.
 # When error with normal way, return dist_crow * AVERAGE_RATIO.
+#AVERAGE_RATIO = 0.76
 AVERAGE_RATIO = 1.0
 
 def average_ratio(dist_crow):
@@ -182,6 +183,7 @@ def test_distances_dbs(nb_tests):
     avg_2di = []
 
     err_count = 0
+    f = open("tests_" + str(AVERAGE_RATIO) + ".csv", mode="w")
 
     for i in range(nb_tests):
         zc_x = zipcodes[randrange(nb_zipcodes)]
@@ -209,6 +211,7 @@ def test_distances_dbs(nb_tests):
             avg_2di.append(dp2di)
 
             print(TABLE % (zc_x.zip_code,zc_x.country_iso,zc_y.zip_code, zc_y.country_iso, math.floor(dist_route), dpcro, dpgr1, dpg10, dp2di))
+            f.write("%s,%s,%s,%s\n" % (dpcro, dpgr1, dpg10, dp2di))
         except Exception as e:
             print((TABLE + " error getting route") % (zc_x.zip_code,zc_x.country_iso,zc_y.zip_code, zc_y.country_iso, "", "", "", "", ""))
             err_count += 1
@@ -219,8 +222,3 @@ def test_distances_dbs(nb_tests):
     print(TABLE % ("","","","","", int(sum(avg_cro)/nb_t_final), int(sum(avg_gr1)/nb_t_final), int(sum(avg_g10)/nb_t_final), int(sum(avg_2di)/nb_t_final)))
 
     print("Number of errors with Graphhopper : " + str(err_count) + " , " + str(int(err_count * 100 / nb_tests)) + "%.")
-
-    with open("tests_" + str(AVERAGE_RATIO) + ".txt", mode="w") as f:
-        avgs = zip(avg_cro,avg_gr1,avg_g10,avg_2di)
-        for avg in avgs:
-            f.write("%s,%s,%s,%s\n" % avg)
